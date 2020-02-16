47bae949310bc5c43ba792b59bd91a4a69f00483
diegovidal <diegovidal08@gmail.com>
2020-02-16 19:30:02 +0000

    + Creating network interceptor

--- app/src/main/java/com/dvidal/samplereviews/core/datasource/remote/NetworkConstants.kt
+++ app/src/main/java/com/dvidal/samplereviews/core/datasource/remote/NetworkConstants.kt
@@ -1,13 +1,11 @@
 package com.dvidal.samplereviews.core.datasource.remote
 
-import com.dvidal.samplereviews.BuildConfig
-
 /**
  * @author diegovidal on 2020-02-16.
  */
 
 object NetworkConstants {
 
-    const val LIMIT_REVIEWS = BuildConfig.LIMIT_REVIEWS
+    const val LIMIT_REVIEWS = 5
     const val SORT_BY_DATE = "date:desc"
 }
--- app/src/main/java/com/dvidal/samplereviews/core/datasource/remote/NetworkInterceptor.kt
+++ app/src/main/java/com/dvidal/samplereviews/core/datasource/remote/NetworkInterceptor.kt
@@ -1,7 +1,26 @@
 package com.dvidal.samplereviews.core.datasource.remote
 
+import com.dvidal.samplereviews.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+
 /**
  * @author diegovidal on 2020-02-15.
  */
-class NetworkInterceptor {
+class NetworkInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response? {
+
+        val modifiedRequest = chain.request()
+            .newBuilder()
+            .addHeader("User-Agent", USER_AGENT_VALUE)
+            .build()
+
+        return chain.proceed(modifiedRequest)
+    }
+
+    companion object {
+
+        val USER_AGENT_VALUE: String = BuildConfig.USER_AGENT_VALUE
+    }
 }
